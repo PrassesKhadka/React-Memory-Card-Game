8d6b69e2ae012d0cda3528a88ea5855d868944c2
Prasses Khadka <khadkaprasses@gmail.com>
2023-07-06 21:56:02 +0545

    Finished designing scoreboard

--- memory-card/src/Components/MainsComponent/Scoreboard.js
+++ memory-card/src/Components/MainsComponent/Scoreboard.js
@@ -4,14 +4,32 @@ import styled from 'styled-components'
 const Scoreboard=({currentscore,bestscore})=>{
     return(
         <Board>
-            <p>Current Score:{currentscore}</p>
-            <p>Best Score:{bestscore}</p>
+            <Current>Current Score:{currentscore}</Current>
+            <Best>Best Score:{bestscore}</Best>
         </Board>
     )
 }
 
 const Board=styled.div`
     background-color:lightgreen;
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+    flex-wrap:wrap;
+`
+const Current=styled.p`
+    border:1px solid black;
+    border-radius:25px;
+    padding:8px;
+    background-color:white;
+    color:blue;
+`
+const Best=styled.p`
+    border:1px solid black;
+    border-radius:25px;
+    padding:8px;
+    background-color:white;
+    color:red;
 `
 
 export default Scoreboard;

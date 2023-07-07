c8b380da50df8b970eacb35618121a7717a87914
Prasses Khadka <khadkaprasses@gmail.com>
2023-07-07 11:58:16 +0545

    Wrap up

--- memory-card/src/Components/Wrapper.js
+++ memory-card/src/Components/Wrapper.js
@@ -3,13 +3,15 @@ import {useState,useEffect} from 'react';
 import styled from 'styled-components';
 import styles from '../../src/App.css';
 
-
+//Popup appear
 const Wrapper=({handleChange})=>{
     return(
         <Instruction>
             <MessageBox>
+                <h2>How to play:</h2>
                 <Message>Don't click on the same card twice.</Message>
                 <Ok onClick={handleChange}>Ok</Ok>
+                <h1>Prasses Khadka</h1>
             </MessageBox>
         </Instruction>
     )
@@ -19,20 +21,19 @@ const Instruction=styled.div`
     position:fixed;
     height:100%;
     width:100%;
-    background-color:red;
-    opacity:0.85;
+    background-color:rgba(255,2255,255,0.3);
     display:flex;
     justify-content:center;
     align-items:center;
     flex-wrap:wrap;
-    `
+`
     
 const MessageBox=styled.div`
     border:1px solid black;
     border-radius:5px;
     height:300px;
     padding:25px;
-    background-color:white;
+    background-color:#fff;
     display:flex;
     flex-direction:column;
     justify-content:center;
@@ -43,6 +44,7 @@ const MessageBox=styled.div`
 const Message=styled.p`
     margin:25px;
     font-weight:bold;
+    color:red;
 `
 
 const Ok=styled.button`
@@ -50,6 +52,7 @@ const Ok=styled.button`
     border-radius:25px;
     height:40px;
     width:60px;
+    margin-bottom:25px;
     background-color:black;
     color:white;
 `
